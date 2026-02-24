aeccdba38f3fa54195c469319b65c423e17a77ae
Keny C <do.not.contact.me@example.com>
2026-02-24 18:21:12 +0000

    Changed regression image file to something Unix-compatible

--- tests/common/utils.rs
+++ tests/common/utils.rs
@@ -1,5 +1,6 @@
 use base64::Engine;
 use rex::font::backend::ttf_parser::TtfMathFont;
+use sha2::Digest;
 
 
 
@@ -14,26 +15,12 @@ pub fn small_ascii_repr(input : &str) -> String {
     let mut to_return = String::new();
     for c in input.chars().take(20) {
         match c {
-            c if 
-                   c.is_ascii_alphanumeric() 
-                || " +:-<>=()_^?!&{}".contains(c) // some allowable innocuous characters
+            c if  c.is_ascii_alphanumeric() || "_-".contains(c) // some allowable innocuous characters
             => {
-                to_return.push(c);
+                to_return.push(c.to_ascii_lowercase());
             },
-            _ => to_return.push(' '),
+            _ => to_return.push('_'),
         };
-    }
-    to_return
-}
-
-
-pub const HASH_SIZE : usize = 8;
-pub fn simple_hash(input : &[u8]) -> [u8; HASH_SIZE] {
-    let mut to_return = [0; HASH_SIZE];
-    for chunk in input.chunks(HASH_SIZE) {
-        for (value, character) in Iterator::zip(to_return.iter_mut(), chunk.into_iter()) {
-            *value = u8::wrapping_add(*value, *character);
-        }
     }
     to_return
 }
@@ -43,9 +30,19 @@ pub fn equation_to_filepath(equation: &str, description : &str) -> String {
     let mut bytes = description.as_bytes().to_vec();
     bytes.extend_from_slice(equation.as_bytes());
 
-    format!(
+    let hash = sha2::Sha256::digest(&bytes);
+    let filename = format!(
         "{}-{}.png",
         &small_ascii_repr(equation),
-        base64::engine::general_purpose::URL_SAFE_NO_PAD.encode(simple_hash(&bytes)),
-    )
+        base64::engine::general_purpose::URL_SAFE_NO_PAD.encode(hash),
+    );
+    
+    // Normally, the 'assert' below should pass. 
+    // small_ascii_repr returns at most 20 bytes
+    // sha256 is at most 32 bytes
+    // base64 is at must 4 / 3 x more than original
+    // So max <= 20 + 1 + 4 / 3 x 32 + 3 < 67
+    assert!(filename.len() < 255);
+    
+    filename
 }
